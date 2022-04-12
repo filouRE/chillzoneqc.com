f79b90b03dc058d5cc3a1acd2812dd45abef0786
Félix <51272629+Ambitionist@users.noreply.github.com>
2022-04-12 19:04:25 -0400

    main

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="w-screen h-screen flex items-center justify-center">
+        <p className=" text-center text-4xl">Hello from chillzone</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/privacy">{/* <About /> */}</Route>
+        <Route path="/tos">{/* <Users /> */}</Route>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
