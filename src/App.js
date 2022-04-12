d3b3e0b9a9af43c7e6e295a39d79dec058cffcc7
Félix <51272629+Ambitionist@users.noreply.github.com>
2022-04-12 19:17:01 -0400

    typo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div className="w-screen h-screen flex items-center justify-center">
-        <p className=" text-center text-4xl">Hello from chillzone</p>
+        <p className=" text-center text-4xl">Hello from ChillzoneQC</p>
       </div>
     </>
   );
