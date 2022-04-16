6c1e4fdf50313881e02361bccf4ea34856a6b7b7
Félix <51272629+Ambitionist@users.noreply.github.com>
2022-04-15 20:39:50 -0400

    save

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,14 @@ import { useEffect, useState } from "react";
 import Burger from "../../data/burger.svg";
 import Exit from "../../data/exit.svg";
 
-const Header = ({ children }) => {
+const Header = ({ children, color = "text-white", filter }) => {
   const [loaded, setLoaded] = useState(false);
   const [mobile, setMobile] = useState(false);
 
   useEffect(() => {
     setMobile(window.innerWidth <= 600);
     setLoaded(true);
+    console.log(color);
   }, []);
 
   const onToggleSidebar = (toggle) => {
@@ -21,11 +22,11 @@ const Header = ({ children }) => {
   if (loaded) {
     return (
       <>
-        <header className="sticky top-0 z-99 py-6 px-6 flex items-center justify-between overflow-x-hidden">
+        <header className="sticky top-0 z-99 py-6 px-5 flex items-center justify-between overflow-x-hidden">
           <a href="/">
-            <a className="text-3xl font-bold text-white">CHILLZONE QC</a>
+            <a className={`text-3xl font-bold ${color}`}>CHILLZONE QC</a>
           </a>
-          {mobile ? <img width={30} height={30} src={Burger} onClick={() => onToggleSidebar(true)} /> : <nav className="flex gap-10  items-center text-white">{children}</nav>}
+          {mobile ? <img width={30} height={30} src={Burger} className={`cursor-pointer ${filter}`} onClick={() => onToggleSidebar(true)} /> : <nav className="flex gap-10  items-center text-white">{children}</nav>}
         </header>
 
         <header className="absolute w-screen h-screen top-0 overflow-hidden translate-x-full">
@@ -33,7 +34,7 @@ const Header = ({ children }) => {
             <a href="/">
               <a className="text-3xl font-bold">CHILLZONE QC</a>
             </a>
-            <img width={30} height={30} src={Exit} onClick={() => onToggleSidebar(false)} />
+            <img width={30} height={30} src={Exit} className="cursor-pointer" onClick={() => onToggleSidebar(false)} />
           </div>
         </header>
       </>
